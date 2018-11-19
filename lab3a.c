7eeef451a527fe6345610d59a55b51d78b76746a
Matthew Paterno <mpaterno@live.com>
2018-11-19 12:40:13 -0800

    Fuse Build Included

--- lab3a.c
+++ lab3a.c
@@ -2,24 +2,26 @@
 // Created by Matei Lupu on 11/17/18.
 //
 #include <stdio.h>
-#include <ext2_fs.h>
+#include "ext2_fs.h"
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #define superblockOffset 1024
-#define groupOffset     1024
+#define groupOffset 1024
 
 struct ext2_super_block superBuffer;
-struct ext2_inode       inodeBuffer;
-struct ext2_group_desc  groupBuffer;
+struct ext2_inode inodeBuffer;
+struct ext2_group_desc groupBuffer;
 
-void createSuperblockSummary(int fd, const char* path)
+void createSuperblockSummary(int fd, const char *path)
 {
-    int superblockFd = creat( path, S_IRWXU ); // file owner has read, write and execute permissions
+    int superblockFd = creat(path, S_IRWXU); // file owner has read, write and execute permissions
 
-    pread( fd, &superBuffer, sizeof(ext2_super_block), superblockOffset )
+    pread(fd, &superBuffer, sizeof(ext2_super_block), superblockOffset);
 
     dprintf(superblockFd, "SUPERBLOCK,");
 
-    dprintf(superblockFd, "%d,", superBuffer.s_blocks_count;
+    dprintf(superblockFd, "%d,", superBuffer.s_blocks_count);
 
     dprintf(superblockFd, "%d,", superBuffer.s_inodes_count);
 
@@ -34,48 +36,47 @@ void createSuperblockSummary(int fd, const char* path)
     dprintf(superblockFd, "%d\n,", superBuffer.s_first_ino);
 
     close(superblockFd);
-
 }
 
-int createGroupSummary(int fd, const char* path) // returns number of froups
+int createGroupSummary(int fd, const char *path) // returns number of froups
 {
     int numberOfGroups = superBuffer.s_blocks_count / superBuffer.s_blocks_per_group + 1;
 
     int blockRemainder = superBuffer.s_blocks_count % superBuffer.s_blocks_per_group;
     int inodeRemainder = superBuffer.s_inodes_count % superBuffer.s_inodes_per_group;
 
-    groupBuffer = malloc(numberOfGroups*sizeof(struct ext2_group_desc));
+    groupBuffer = malloc(numberOfGroups * sizeof(struct ext2_group_desc));
 
-    int groupFd = creat( path, S_IRWXU );
+    int groupFd = creat(path, S_IRWXU);
 
     int i = 0;
-    for(; i < numberOfGroups; i++)
+    for (; i < numberOfGroups; i++)
     {
-        pread( fd, &groupBuffer[i], sizeof(ext2_group_desc), superblockOffset + groupOffset + sizeof(struct ext2_group_desc)*i )
+        pread(fd, &groupBuffer[i], sizeof(ext2_group_desc), superblockOffset + groupOffset + sizeof(struct ext2_group_desc) * i)
 
-        dprintf(groupFd, "GROUP,");
+            dprintf(groupFd, "GROUP,");
 
         dprintf(groupFd, "%d,", i);
 
-        if( blockRemainder == 0 )
+        if (blockRemainder == 0)
         {
             dprintf(groupFd, "%d,", superBuffer.s_blocks_per_group);
         }
         else
         {
-            if( (i-1) == 0 )
+            if ((i - 1) == 0)
             {
                 dprintf(groupFd, "%d,", blockRemainder);
             }
         }
 
-        if( inodeRemainder == 0 )
+        if (inodeRemainder == 0)
         {
             dprintf(groupFd, "%d,", superBuffer.s_inodes_per_group);
         }
         else
         {
-            if( (i-1) == 0 )
+            if ((i - 1) == 0)
             {
                 dprintf(groupFd, "%d,", inodeRemainder);
             }
@@ -90,84 +91,78 @@ int createGroupSummary(int fd, const char* path) // returns number of froups
         dprintf(groupFd, "%d,", groupBuffer[i].bg_inode_bitmap);
 
         dprintf(groupFd, "%d\n,", groupBuffer[i].bg_inode_table);
-
     }
 
     close(groupFd);
 }
 
-void createFreeSummary(int fd, const char* groupPath, const char* inodePath int numOfGroups)
+void createFreeSummary(int fd, const char *groupPath, const char *inodePath int numOfGroups)
 {
-    int freeGroupFd = creat( groupPath, S_IRWXU );
-    int freeInodeFd = creat( inodePath, S_IRWXU );
+    int freeGroupFd = creat(groupPath, S_IRWXU);
+    int freeInodeFd = creat(inodePath, S_IRWXU);
 
     uint8_t uint8Buffer;
 
     int i = 0;
-    for(; i < numOfGroups; i++ )
+    for (; i < numOfGroups; i++)
     {
         int blockSize = 1024 << superBuffer.s_log_block_size;
 
         int j = 0;
-        for(; j < blockSize; j++)
+        for (; j < blockSize; j++)
         {
             int bitmapLocation = EXT2_MIN_BLOCK_SIZE << superBuffer.s_log_block_size;
 
-            pread( fd, &uint8Buffer, 1, groupBuffer[i].bg_block_bitmap * bitmapLocation + j );
+            pread(fd, &uint8Buffer, 1, groupBuffer[i].bg_block_bitmap * bitmapLocation + j);
 
             int k = 0;
-            for(; k < 8; k++)
+            for (; k < 8; k++)
             {
-                if( (uint8Buffer & 1 << k) == 0 ) // 0 means free
+                if ((uint8Buffer & 1 << k) == 0) // 0 means free
                 {
-                    dprintf( freeGroupFd, "BFREE,");
+                    dprintf(freeGroupFd, "BFREE,");
 
-                    int blockNumber = i*superBuffer.s_blocks_per_group + j*8 + k + 1;
-                    dprintf( freeGroupFd, "%d\n,", blockNumber);
+                    int blockNumber = i * superBuffer.s_blocks_per_group + j * 8 + k + 1;
+                    dprintf(freeGroupFd, "%d\n,", blockNumber);
                 }
             }
 
-            pread( fd, &uint8Buffer, 1, groupBuffer[i].bg_inode_bitmap * bitmapLocation + j );
+            pread(fd, &uint8Buffer, 1, groupBuffer[i].bg_inode_bitmap * bitmapLocation + j);
 
             k = 0;
-            for(; k < 8; k++)
+            for (; k < 8; k++)
             {
-                if( (uint8Buffer & 1 << k) == 0 ) // 0 means free
+                if ((uint8Buffer & 1 << k) == 0) // 0 means free
                 {
-                    dprintf( freeInodeFd, "IFREE,");
+                    dprintf(freeInodeFd, "IFREE,");
 
-                    int inodeNumber = i*superBuffer.s_inodes_per_group + j*8 + k + 1;
-                    dprintf( freeInodeFd, "%d\n,", inodeNumber);
+                    int inodeNumber = i * superBuffer.s_inodes_per_group + j * 8 + k + 1;
+                    dprintf(freeInodeFd, "%d\n,", inodeNumber);
                 }
             }
-
         }
-
     }
 
     close(freeGroupFd);
     close(freeInodeFd);
 }
 
-void createInodeSummary(int fd, const char* path, numOfGroups)
+void createInodeSummary(int fd, const char *path, numOfGroups)
 {
-
 }
 
-
-
-int main( int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 
-    const char* superPath      = "superCSV.csv";
-    const char* groupPath      = "groupCSV.csv";
-    const char* freeGroupPath  = "freeGCSV.csv";
-    const char* freeInodePath  = "freeICSV.csv";
-    const char* inodePath      = "inodeCSV.csv";
+    const char *superPath = "superCSV.csv";
+    const char *groupPath = "groupCSV.csv";
+    const char *freeGroupPath = "freeGCSV.csv";
+    const char *freeInodePath = "freeICSV.csv";
+    const char *inodePath = "inodeCSV.csv";
 
-    char* filename;
+    char *filename;
 
-    if(argv[1] != NULL)
+    if (argv[1] != NULL)
     {
         char *filename = argv[1];
     }
@@ -178,43 +173,37 @@ int main( int argc, char* argv[])
     }
 
     int fd = open(filename, O_RDONLY);
-    if( fd < 0 )
+    if (fd < 0)
     {
-        fprintf( stderr, "Could not open file with specified image" );
+        fprintf(stderr, "Could not open file with specified image");
         exit(1);
     }
-
 
     createSuperblockSummary(fd, superPath);
     int numberOfGroups = createGroupSummary(fd, groupPath);
     createFreeSummary(fd, freeGroupPath, freeInodePath, numberOfGroups);
-//  createInodeSummary(fd, inodePath, numberOfGroups);
+    //  createInodeSummary(fd, inodePath, numberOfGroups);
 
     close(fd);
 
-//    SUPERBLOCK
-//    total number of blocks (decimal)
-//    total number of i-nodes (decimal)
-//    block size (in bytes, decimal)
-//    i-node size (in bytes, decimal)
-//    blocks per group (decimal)
-//    i-nodes per group (decimal)
-//    first non-reserved i-node (decimal)
+    //    SUPERBLOCK
+    //    total number of blocks (decimal)
+    //    total number of i-nodes (decimal)
+    //    block size (in bytes, decimal)
+    //    i-node size (in bytes, decimal)
+    //    blocks per group (decimal)
+    //    i-nodes per group (decimal)
+    //    first non-reserved i-node (decimal)
 
-
-
-//    GROUP
-//    group number (decimal, starting from zero)
-//    total number of blocks in this group (decimal)
-//    total number of i-nodes in this group (decimal)
-//    number of free blocks (decimal)
-//    number of free i-nodes (decimal)
-//    block number of free block bitmap for this group (decimal)
-//    block number of free i-node bitmap for this group (decimal)
-//    block number of first block of i-nodes in this group (decimal)
-
-
-
+    //    GROUP
+    //    group number (decimal, starting from zero)
+    //    total number of blocks in this group (decimal)
+    //    total number of i-nodes in this group (decimal)
+    //    number of free blocks (decimal)
+    //    number of free i-nodes (decimal)
+    //    block number of free block bitmap for this group (decimal)
+    //    block number of free i-node bitmap for this group (decimal)
+    //    block number of first block of i-nodes in this group (decimal)
 
     return 0;
 }
